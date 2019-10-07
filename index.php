be149a4b8752ce7f548e327687a25da5d97ae937
Lisa LONCHAMP <lloncham@e1r5p19.42.fr>
2019-10-07 13:50:36 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+    <meta charset="utf-8"/>
+        <title>Camagru</title>
+        <style>
+        </style>
+</head>
+<body>
+<h1>CAMAGRU</h1>
+</body>
