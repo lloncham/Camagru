ba0fc4877b8ebcc8368e9c15a6c5ff73bf7069c8
Lisa LONCHAMP <lloncham@e1r9p15.42.fr>
2019-10-09 16:08:45 +0200

    pdo ok

---
+++ pdo.php
@@ -0,0 +1,18 @@
+<?php
+
+function    dbconnect()
+{
+    $user = 'root';
+    $pass = 'lisalol';
+    $option = 
+    try {
+        $db = new PDO('mysql:host=localhost:3306;dbname=camagru', $user, $pass);
+        return $db;
+    } catch (Exception $e) {
+        die('Erreur : '.$e->getMessage());
+    }
+}
+
+// foreach($dbh->query('SELECT * from COMTE') as $row) {
+//     print_r($row);
+?>
